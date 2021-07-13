883844a8385ba45bb1e37fa99271759ff1aeed5d
Frederick Kobo <frederick.kobo@ebucks.com>
2021-07-13 16:32:38 +0200

    Initial commit

---
+++ app/src/main/java/za/co/fredkobo/aboutme/MainActivity.kt
@@ -0,0 +1,38 @@
+package za.co.fredkobo.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import za.co.fredkobo.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("John Black")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/za/co/fredkobo/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package za.co.fredkobo.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
